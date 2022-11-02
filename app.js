24b11c1e70235854d45b4d0da77976727b2823e6
Taylor King <taylorking@taylors-mbp.lan>
2022-11-02 17:18:57 -0400

    organizing material with css

---
+++ app.js
@@ -0,0 +1,50 @@
+$(() => {
+
+$('div').on('click', function() {
+    $(this).toggleClass('show-description');
+})
+
+})
+
+
+
+
+
+//Carousel////Carousel////Carousel////Carousel//
+
+$(() => {
+
+  let currentImgIndex = 0;
+  let numOfImages = $('.carousel-images').children().length - 1
+
+
+
+$('.next').on('click', () => {
+ $('.carousel-images').children().eq(currentImgIndex).css('display', 'none')
+
+ if(currentImgIndex < numOfImages) {
+  currentImgIndex ++
+ } else {
+  currentImgIndex = 0
+ }
+
+ $('.carousel-images').children().eq(currentImgIndex).css('display', 'block')
+})
+
+
+$('.previous').on('click', () => {
+$('.carousel-images').children().eq(currentImgIndex).css('display', 'none')
+
+if(currentImgIndex > 0) {
+  currentImgIndex --
+} else {
+  currentImgIndex = numOfImages
+}
+
+$('.carousel-images').children().eq(currentImgIndex).css('display', 'block')
+
+})
+
+});
+
+//Carousel////Carousel////Carousel////Carousel//
