2121ceda2dc9761f4dd0ab767c84272c7a6ab03e
Taylor King <taylorking@taylors-mbp.lan>
2022-11-03 17:48:06 -0400

    CSS and Jquery

--- app.js
+++ app.js
@@ -48,3 +48,47 @@ $('.carousel-images').children().eq(currentImgIndex).css('display', 'block')
 });
 
 //Carousel////Carousel////Carousel////Carousel//
+
+
+
+$(() => {
+
+
+  $('#bio').hide()
+  $('#resume').hide()
+  $('#projects').hide()
+
+
+const show = () => {
+  $('#greeting').toggle()
+  $('#bio').hide()
+  $('#resume').hide()
+  $('#projects').hide()
+}
+
+const show2 = () => {
+  $('#bio').toggle()
+  $('#greeting').hide()
+  $('#resume').hide()
+  $('#projects').hide()
+}
+const show3 = () => {
+  $('#resume').toggle()
+  $('#bio').hide()
+  $('#greeting').hide()
+  $('#projects').hide()
+}
+const show4 = () => {
+  $('#projects').toggle()
+  $('#bio').hide()
+  $('#resume').hide()
+  $('#greeting').hide()
+}
+
+$('#h').on('click',show)
+$('#b').on('click',show2)
+$('#r').on('click',show3)
+$('#p').on('click',show4)
+
+
+})
